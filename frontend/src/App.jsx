696c21cb4b6e115eeb69096321c7530c839ecc89
Hadi-SayedAhmad <121687873+Hadi-SayedAhmad@users.noreply.github.com>
2023-10-04 15:23:19 +0300

    Header done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <Outlet></Outlet>
+    <>
+    <Header />
+      <Outlet></Outlet>
+    </>
+    
   )
 }
 
