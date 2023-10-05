762fbe7bbaf5385cb1b28472f271e40d2544a44f
Hadi-SayedAhmad <121687873+Hadi-SayedAhmad@users.noreply.github.com>
2023-10-05 13:56:38 +0300

    sign up functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
+import {toast, ToastContainer} from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   return (
     <>
     <Header />
       <Outlet></Outlet>
+      <ToastContainer />
     </>
     
   )
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,15 +1,59 @@
-import {Link} from "react-router-dom"
-
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { toast } from "react-toastify"
+import { useNavigate } from "react-router-dom";
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true)
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      })
+      
+      const data = await res.json()
+      if (data.success === false) {
+        setLoading(false)
+        toast.error(data.message)
+        return 
+      } 
+      
+      toast.success(data);
+      setLoading(false)
+      navigate("/sign-in");
+      // console.log(data);
+    } catch (err) {
+      setLoading(false)
+      toast.error(err.message)
+    }
+
+  }
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form autoComplete='off' className='flex flex-col gap-3'>
-        <input type='text' placeholder='Username' className='border p-3 rounded-lg' id='username' />
-        <input type='email' placeholder='Email' className='border p-3 rounded-lg' id='email' />
-        <input type='password' placeholder='Password' className='border p-3 rounded-lg' id='password' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80'>Sign Up</button>
-        
+      <form autoComplete='off' className='flex flex-col gap-3' onSubmit={handleSubmit}>
+        <input type='text' placeholder='Username' className='border p-3 rounded-lg focus:outline-none' id='username' onChange={handleChange} />
+        <input type='email' placeholder='Email' className='border p-3 rounded-lg focus:outline-none' id='email' onChange={handleChange} />
+        <input type='password' placeholder='Password' className='border p-3 rounded-lg focus:outline-none' id='password' onChange={handleChange} />
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80'>{loading ? "LOADING..." : "Sign Up"}</button>
+
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Already have an account?</p>
@@ -17,6 +61,7 @@ export default function SignUp() {
           <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
+
     </div>
   )
 }
